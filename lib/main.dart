9a8b563a7b987a1e48e6643fd376c200764937d9
crizleo <christianleonardo2002@gmail.com>
2024-11-24 20:56:28 -0500

    prueba 927633

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:who/screens/loginPage.dart';
 
 void main() async {
